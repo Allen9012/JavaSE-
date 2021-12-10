40e62e97b9f26dd57e545753fef2ec697960877d
Allen <2439506288@qq.com>
2021-12-10 13:04:51 +0800

    12、10 面向对象编程中级作业5 6

---
+++ homework/Homework05.java
@@ -0,0 +1,30 @@
+package com.homework;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Allen
+ * Date: 2021-11-28
+ * Time: 14:52
+ */
+public class Homework05 {
+    public static void main(String[] args) {
+        Circle circle = new Circle(20);
+        double aera=circle.area();
+        double Perimeter= circle.Perimeter();
+        System.out.println("aera="+aera);
+        System.out.println("Perimeter="+Perimeter);
+    }
+}
+class Circle{
+    double radius;
+    public Circle(double radius){
+        this.radius=radius;
+    }
+    public double area(){
+        return Math.PI*radius*radius;
+    }
+    public double Perimeter(){
+        return 2*Math.PI*radius;
+    }
+}
---
+++ homework/Homework06.java
@@ -0,0 +1,46 @@
+package com.homework;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Allen
+ * Date: 2021-11-28
+ * Time: 14:59
+ */
+public class Homework06 {
+    public static void main(String[] args) {
+        Cale cale = new Cale(2,0);
+        System.out.println("和"+cale.sum());
+        System.out.println("差"+cale.minus());
+        System.out.println("乘"+cale.mul());
+        Double aDouble = cale.div();
+        if(aDouble!=null){
+            System.out.println("除"+ cale.div());
+        }
+    }
+}
+class Cale{
+    double num1;
+    double num2;
+    public Cale(double num1,double num2){
+        this.num1=num1;
+        this.num2=num2;
+    }
+    public double sum(){
+        return num1+num2;
+    }
+    public double minus(){
+        return num1-num2;
+    }
+    public double mul(){
+        return num1*num2;
+    }
+    public Double div(){
+        if(num2==0){
+            System.out.println("num2不能为0");
+            return null;
+        }else{
+            return num1/num2;
+        }
+    }
+}
