e276f538bf24d4127e8c211598ecc08331496fb6
Allen <2439506288@qq.com>
2021-12-21 16:35:03 +0800

    12、21 learning main method

---
+++ Main/Main02.java
@@ -0,0 +1,16 @@
+package com.HigherOrder.Main;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Allen
+ * Date: 2021-12-14
+ * Time: 23:17
+ */
+public class Main02 {
+    public static void main(String[] args) {
+        for (int i = 0; i <args.length ; i++) {
+            System.out.println("args["+i+"] = "+args[i]);
+        }
+    }
+}
