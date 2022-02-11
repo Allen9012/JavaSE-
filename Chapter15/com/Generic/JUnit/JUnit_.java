b840668a2658728eb3e5f32e7648a53575461fb1
Allen <2439506288@qq.com>
2022-02-11 11:04:51 +0800

    2022-02-11 learning Junit

---
+++ Chapter15/com/Generic/JUnit/JUnit_.java
@@ -0,0 +1,25 @@
+package com.Generic.JUnit;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Allen
+ * Date: 2022-01-29
+ * Time: 20:19
+ */
+public class JUnit_ {
+    public static void main(String[] args) {
+//        new JUnit_().m1();
+
+    }
+    @Test
+    public void m1(){
+        System.out.println("m1 is called");
+    }
+    @Test
+    public void m2(){
+        System.out.println("m2 is called");
+    }
+}
