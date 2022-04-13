a80c79eb4224c6e83fbad5d9f23ae1583581850b
Allen <2439506288@qq.com>
2022-04-13 10:28:45 +0800

    2022-04-13 上传Cat类

---
+++ Chapter23/Cat.java
@@ -0,0 +1,18 @@
+package com;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Allen
+ * Date: 2022-04-07
+ * Time: 18:59
+ */
+public class Cat {
+    private String name="招财猫" ;
+    public void hi(){
+        System.out.println("hi "+ name);
+    }
+    public void cry(){
+        System.out.println(name+ "喵喵叫");
+    }
+}
